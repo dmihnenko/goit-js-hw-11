adab46fb79f152bee2a841f489d5aeb386bb0dcd
dmihnenko <110346306+dmihnenko@users.noreply.github.com>
2023-06-14 01:46:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ async function onSearchForm(evt) {
     } else {
       gallery.insertAdjacentHTML("beforeend", markup(images.hits));
       loadMoreBtn.classList.remove("is-hidden");
-      Notiflix.Notify.success(`We found ${images.hits} images.`);
+      Notiflix.Notify.success(`We found ${images.totalHits} images.`);
     }
   }
 }
