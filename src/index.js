128f808a928315e97d7a749774132d5bddd3eeb9
dmihnenko <110346306+dmihnenko@users.noreply.github.com>
2023-06-14 00:21:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ let total ;
 let observer;
 let isEnd;
 let query;
-let imgId = 0;
+let imgId;
 let lastObserver;
 
 
@@ -72,7 +72,7 @@ function buildMarkup(obj) {
 }
 function addMarkup(markup) {
   refs.gallery.insertAdjacentHTML('beforeend', markup);
-  if (imgId <= total) {
+  if (imgId >= total) {
     createLastObserver();
   }
   window.scrollBy({
@@ -85,7 +85,7 @@ function onLoad(entries, observer) {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.remove();
-      page += 1;
+     
       fetchImages(query, page)
         .then(data => {
           addMarkup(buildMarkup(data.hits));
